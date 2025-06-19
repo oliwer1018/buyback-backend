58e3cddf3a68dcb7f6d2363c23164e20e87ed7a7
oliwer1018 <helloworld222414@gmail.com>
2025-06-19 21:16:27 +0800

    django setup inside celery_app

--- app/celery_app/__init__.py
+++ app/celery_app/__init__.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import django
 from celery import Celery
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Ensures Django settings are loaded
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+django.setup()
 
 celery_app = Celery(
     "buyback",
